c6efd0fc59365e259e4a4197dbe59da7f96416e3
Arthur Reyes <142542790+Arthur01012@users.noreply.github.com>
2026-09-08 21:49:02 -0600

    carga del proyecto con los requrimientos indicados en la pagina web

---
+++ src/com/uthh/edd/unidad1/arreglos/PruebaEntorno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uthh.edd.unidad1.arreglos;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class PruebaEntorno {
+    public   static void main(String[] args){
+        System.out.println("Entorno configurado correctamente");
+    }
+}
